8f3cc1ba75031aabfacefc8cd749bc153a8b96ef
Evan Voeltner <evanvoeltner@gmail.com>
2022-08-25 11:09:38 -0500

    feat: completed the robot attack logic with printouts of the attack results

--- robot.py
+++ robot.py
@@ -7,4 +7,6 @@ class Robot:
         self.active_weapon = Weapon()
 
     def attack(self, dinosaur):
-        pass
+        dinosaur.health -= self.active_weapon.attack_power
+        print(f'Robot attacked Dinosaur with {self.active_weapon.name}, and it delt {str(self.active_weapon.attack_power)}!')
+        print(f'Dinosaur has {str(dinosaur.health)} health left!')
