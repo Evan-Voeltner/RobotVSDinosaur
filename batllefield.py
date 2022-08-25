b9ec7fc48bbab9e6f877e7574dae6179cc3c7925
Evan Voeltner <evanvoeltner@gmail.com>
2022-08-25 11:00:49 -0500

    feat: established every class that will be used in the game

---
+++ batllefield.py
@@ -0,0 +1,7 @@
+from dinosaur import Dinosaur
+from robot import Robot
+
+class Battlefield:
+    def __init__(self):
+        self.robot = Robot()
+        self.dinosaur = Dinosaur()
---
+++ dinosaur.py
@@ -0,0 +1,8 @@
+class Dinosaur:
+    def __init__(self):
+        self.name = ''
+        self.health = 100
+        self.attack_power = 50
+
+    def attack(self, robot):
+        pass
